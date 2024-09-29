ce961efbd27caea2a5668358c53ee4757022c3f6
Kevin Ramos Rivas <mc6.62x10.34@gmail.com>
2024-09-29 17:49:59 -0500

    fix(frontend): Soluciona un error que mostraba el carrito de compras abierto cuando se redirigia a otra pantalla

--- src/app/home/components/shop-car/shop-car.component.ts
+++ src/app/home/components/shop-car/shop-car.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { ShopCarService } from '../../services/shop-car.service';
 import { Product } from '../../interfaces/products.interface';
 
@@ -19,7 +19,10 @@ export class ShopCarComponent {
 
   public total = this.shopCarService.total$;
   
-  public itemsCount = this.shopCarService.numItems$; 
+  public itemsCount = this.shopCarService.numItems$;
+  private elementRef = inject(ElementRef);
+  @ViewChild('productsList') productsList: ElementRef = this.elementRef.nativeElement;
+  @ViewChild('buttonProductsList') buttonProductsList: ElementRef = this.elementRef.nativeElement;
 
   public addProductToShopCar(product: Product) {
     this.shopCarService.addProduct({ product, quantity: 1 });
@@ -27,5 +30,10 @@ export class ShopCarComponent {
 
   public removeProductFromShopCar(product: Product) {
     this.shopCarService.removeProduct({ product, quantity: 1 });
+  }
+
+  public closeShopCar($event: any) {
+    this.productsList.nativeElement.classList.remove('show');
+    this.buttonProductsList.nativeElement.classList.remove('show');
   }
 }
